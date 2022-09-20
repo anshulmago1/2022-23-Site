f46b542f1250e9f4b4ecb891e2ee49595028ceff
aaronthechen <aaronchen31415@gmail.com>
2022-09-20 10:09:48 -0500

    mobile scaling for club info, adjust project title

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button({ children }) {
   return (
-    <button className='bg-indigo-700 w-fit px-4 py-2 text-xl rounded-lg hover:bg-indigo-900 transition-all'>{children}</button>
+    <button className='bg-indigo-700 w-fit px-4 py-2 sm:text-xl rounded-lg hover:bg-indigo-900 transition-all'>{children}</button>
   )
 }
 
--- src/Pages/ClubInfo.jsx
+++ src/Pages/ClubInfo.jsx
@@ -5,15 +5,15 @@ function ClubInfo() {
   return (
     <div className="flex flex-col items-center">
       <div className="z-10 max-w-7xl py-48 px-8">
-        <div className="w-full max-h-screen flex flex-col">
-          <h1 className="text-7xl">What is CSNHS?</h1>
-          <p className="py-10 text-xl">
+        <div className="w-full max-h-screen flex flex-col sm:gap-10 gap-4">
+          <h1 className="md:text-7xl sm:text-6xl text-5xl">What is CSNHS?</h1>
+          <p className="sm:text-xl">
             Computer Science National Honor Society is a club focused on teaching a variety of topics in computer science,
             along with helping the community with tech-related events and projects.
             We strive to provide students with a chance to learn valuable skills and to help others.
           </p>
-          <h1 className="text-7xl">Why should I join?</h1>
-          <p className="py-10 text-xl">
+          <h1 className="md:text-7xl sm:text-6xl text-5xl">Why should I join?</h1>
+          <p className="sm:text-xl">
             If you are interested in software development, game design, or pretty much any field of computer science,
             our club is a great way to find ways to learn and apply these skills in the real world through our volunteering opportunities.
             The concepts and topics we teach are valuable tools if you plan on becoming a software developer or just want to build games and projects of your own.
