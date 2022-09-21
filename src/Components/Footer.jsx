e2eaa300f6a63ab395c65095fc0cf5c01d9b648f
Leo Zhang <leoyzhang06@gmail.com>
2022-09-21 09:28:48 -0500

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,13 +6,13 @@ function Footer() {
       <footer className="p-6 text-sm">
         <div className="flex items-center mb-2">
           <div className="mr-8">
-            <a href="#" className="hover:text-blue-400 transition-all">Join Our Club</a>
+            <a href="#" className="hover:text-blue-400 transition-all" target="_blank">Join Our Club</a>
           </div>
-          <a href="https://www.facebook.com/groups/chscsnhs" className="flex items-center mr-8 hover:text-blue-400 transition-all">
+          <a href="https://www.facebook.com/groups/chscsnhs" target="_blank" className="flex items-center mr-8 hover:text-blue-400 transition-all">
             <img src={FacebookIcon} alt="Facebook" className="w-6 mr-2" />
             <span>Clements CSNHS</span>
           </a>
-          <a href="https://www.remind.com/join/chscsnhs" className="flex items-center hover:text-blue-400 transition-all">
+          <a href="https://www.remind.com/join/chscsnhs" target="_blank" className="flex items-center hover:text-blue-400 transition-all">
             <img src={RemindIcon} alt="Remind" className="w-7 mr-1" />
             <span>@chscsnhs</span>
           </a>
